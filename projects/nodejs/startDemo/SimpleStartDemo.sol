f831f7ccf483cec0826a63a13803f95e8b41239d
Cristic <CristicMay@gmail.com>
2018-01-17 17:41:52 +0800

    Update SimpleStartDemo.sol

--- projects/nodejs/startDemo/SimpleStartDemo.sol
+++ projects/nodejs/startDemo/SimpleStartDemo.sol
@@ -3,11 +3,18 @@ pragma solidity ^0.4.2;
 contract SimpleStartDemo {
     int256 storedData;
     event AddMsg(address indexed sender, bytes32 msg);
+
+    modifier only_with_at_least(int x) {
+       if (x >= 5) {
+         x = x+10;
+          _;
+       }
+    } 
     function SimpleStartDemo() {
         storedData = 2;
     }
 
-    function setData(int256 x) public {
+    function setData(int256 x) public only_with_at_least(x){
         storedData = x;
         AddMsg(msg.sender, "[in the set() method]");
     }
