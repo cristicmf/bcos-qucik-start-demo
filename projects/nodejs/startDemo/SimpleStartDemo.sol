51903a3f279e4e9256078a7f3271c32f106deec2
Cristic <CristicMay@gmail.com>
2018-01-17 16:45:46 +0800

    Update SimpleStartDemo.sol

--- projects/nodejs/startDemo/SimpleStartDemo.sol
+++ projects/nodejs/startDemo/SimpleStartDemo.sol
@@ -3,9 +3,6 @@ pragma solidity ^0.4.2;
 contract SimpleStartDemo {
     int256 storedData;
     event AddMsg(address indexed sender, bytes32 msg);
-    modifier checkType(status) {
-        if (status!=10) throw;
-    }
     function SimpleStartDemo() {
         storedData = 2;
     }
